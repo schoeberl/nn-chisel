019aaf6164718f0fac23d3ee306002415d4be504
Martin Schoeberl <martin@jopdesign.com>
2023-09-27 14:51:51 +0200

    Multilayer Perceptron

--- src/main/scala/MultilayerPerceptron.scala
+++ src/main/scala/MultilayerPerceptron.scala
@@ -16,6 +16,31 @@ object MultilayerPerceptron extends App {
     compute(in)
     val out = output.output
     val err = exp - out
-    output.weights = output.weights.zip(hidden.map(_.output)).map { case (w, o) => w + err * o }
+    println(s"exp: $exp, out: $out, err: $err")
+    // output.weights = output.weights.zip(hidden.map(_.output)).map { case (w, o) => w + err * o }
+    for (i <- 0 until hidden.length) {
+      output.weights(i) += err * hidden(i).output
+    }
+    output.weights(hidden.length) += err // * output.bias
   }
+
+  def test() = {
+    compute(Array(0, 0))
+    println("0,0 -> " + (output.output + 0.5).toInt + "      "+ output.output)
+    compute(Array(0, 1))
+    println("0,1 -> " + (output.output + 0.5).toInt + "      "+ output.output)
+    compute(Array(1, 0))
+    println("1,0 -> " + (output.output + 0.5).toInt + "      "+ output.output)
+    compute(Array(1, 1))
+    println("1,1 -> " + (output.output + 0.5).toInt + "      "+ output.output)
+  }
+
+  // XOR does not work
+  val input = Array[(Array[Double], Double)]((Array(0, 0), 0), (Array(0, 1), 1), (Array(1, 0), 1), (Array(1, 1), 1))
+
+    for (i <- 0 until 1000) {
+      input.foreach { case (in, exp) => learn(in, exp) }
+      test()
+      println(output.weights.mkString(" "))
+    }
 }
